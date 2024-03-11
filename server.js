f37db51cb73abfe1756b1ec93b036daab815aa8e
Akmal <115373618+D4RKSCR1PTS@users.noreply.github.com>
2024-03-10 23:07:48 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
 import CloudFreed from "./CloudFreed.js";
-import TorControl from 'tor-control';
 
-// Connect to the Tor control port
-var control = new TorControl({
-    password: "D4RKWASHERE",
-    host: "127.0.0.1"
-});
- 
-control.signalNewnym(function (err, status) { // Get a new circuit
-   if (err) {
-      return console.error(err);
-   }
-   console.log(status.messages[0]); // --> "OK"
-});
+const url = "bloxmoon.com"
 
-const cf_clearences = await CloudFreed("bloxmoon.com", "socks5://127.0.0.1:9050")
-console.log( cf_clearences)
+const cf = await CloudFreed(url)
+console.log(cf)
